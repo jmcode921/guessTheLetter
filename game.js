2a2a3dfeceb8f77bed8231a8cce50a044b01aa51
Jesus Morales <jesusmorales7499@gmail.com>
2022-07-06 11:02:11 -0400

    connect the game page to the home page

--- game.js
+++ game.js
@@ -1,16 +1,41 @@
-const word = document.getElementById('word')
+let howManyWrong = document.getElementById('howManyWrong')
 const wrongLetter =document.getElementById('wrongLetters')
+const word = document.getElementById('word')
 const gameWords = [
     "crazy",
     "party",
     "jokes"
 ]
-let howManyWrong = document.getElementById('howManyWrong')
-let amountOfLives = document.getElementById('amountOfLives').innerHTML
+
 let answer =gameWords[Math.floor(Math.random() * gameWords.length)]
  
 
 document.getElementById("myButton").onclick = function () {
     let guessedLetters = document.getElementById("letterGuessed").value;
     console.log(guessedLetters)
+}
+
+function showUp() {
+    let val= document.getElementById("letterGuessed").value
+    if (document.getElementById(answer.value === "crazy")) {
+       if (val === "c"||val==="r"||val==="a"||val==="z"||val==="y"){
+        word.push("letterGuessed") 
+       } else {
+        wrongLetter.push(guessedLetters)
+       }
+    } else if (document.getElementById(answer.value === "party")) {
+        if (val==="p"||val==="a"||val==="r"||val==="t"||val==="y"){
+        word.push(guessedLetters)
+        }else {
+        wrongLetter.push(guessedLetters)
+        }
+    } else if (document.getElementById(answer.value === "jokes")){
+        if (val=== "j"||val==="o"||val==="k"||val==="e"||val==="s"){
+        word.push(guessedLetters)
+        }else {
+        wrongLetter.push(guessedLetters)
+    }
+
+    }
+
 }
